08decbd276dc01f2a40a2cf23a8b8888ec9801ab
mrmuh <alex3.dr@gmail.com>
2015-11-17 20:02:44 +0100

    * basic angular app * translations * browser sync supports html5 mode

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -1,3 +1,6 @@
 /// <reference path="angularjs/angular.d.ts" />
 /// <reference path="jquery/jquery.d.ts" />
 /// <reference path="lodash/lodash.d.ts" />
+/// <reference path="angular-ui-router/angular-ui-router.d.ts" />
+/// <reference path="restangular/restangular.d.ts" />
+/// <reference path="angular-translate/angular-translate.d.ts" />
